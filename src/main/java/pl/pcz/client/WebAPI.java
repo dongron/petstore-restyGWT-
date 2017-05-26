e0bcb658ff3bc2cbf5004566e1137a36ab0944ec
Irek Szcześniak <irek.szczesniak@gmail.com>
2017-05-26 23:59:56 +0200

    WebAPI

---
+++ src/main/java/pl/pcz/client/WebAPI.java
@@ -0,0 +1,15 @@
+package pl.pcz.client;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import org.fusesource.restygwt.client.Method;
+import org.fusesource.restygwt.client.MethodCallback;
+import org.fusesource.restygwt.client.RestService;
+
+public interface WebAPI extends RestService {
+    @GET
+    @Path("http://petstore.swagger.io/v2/pet/{id}")
+    public void getPet(@PathParam("id") int id, MethodCallback<String> m);
+}
--- src/main/java/pl/pcz/client/petstore.java
+++ src/main/java/pl/pcz/client/petstore.java
@@ -1,6 +1,7 @@
 package pl.pcz.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.core.client.Scheduler;
@@ -12,6 +13,17 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
+
+import org.fusesource.restygwt.client.Method;
+import org.fusesource.restygwt.client.MethodCallback;
+import org.fusesource.restygwt.client.Defaults;
+    
 public class petstore implements EntryPoint, Scheduler.RepeatingCommand {
 
     private Label header = new Label();
@@ -19,6 +31,7 @@ public class petstore implements EntryPoint, Scheduler.RepeatingCommand {
     private Label id = new Label("123");
     private Label name = new Label("Spot");
     private Label category = new Label("reserved");
+    private FlowPanel footer = new FlowPanel();
     private int i = 0;
 
     public boolean execute() {
@@ -51,6 +64,9 @@ public class petstore implements EntryPoint, Scheduler.RepeatingCommand {
     }
 
     public void onModuleLoad() {
+
+        Defaults.setAddXHttpMethodOverrideHeader(false);
+            
 	Scheduler.get().scheduleFixedPeriod(this, 1000);
 	
 	// Sample pet data.
@@ -82,14 +98,42 @@ public class petstore implements EntryPoint, Scheduler.RepeatingCommand {
 	FlowPanel footer = new FlowPanel();
 	footer.addStyleName("footer");
 	footer.add(new Label("Kontakt: "));
-	footer.add(new Anchor("office@petstore.pcz.pl",
-			      "mailto: office@petstore.pcz.pl"));
 
+        RequestBuilder builder = new RequestBuilder(RequestBuilder.GET,
+                                                    URL.encode("http://petstore.swagger.io/v2/pet/1"));
+
+        try {
+            Request request = builder.sendRequest(null, new RequestCallback() {
+
+                    public void onError(Request request, Throwable exception) {
+                        footer.add(new Label("Exception caught"));
+                    }
+
+                    public void onResponseReceived(Request request, Response response) {
+                        // footer.add(new Label(response.getText()));
+                    }
+                });
+        } catch (RequestException e) {
+            footer.add(new Label("Exception caught"));
+        }
+        
 	VerticalPanel vp = new VerticalPanel();
 	vp.addStyleName("outer");
 	vp.add(header);
 	vp.add(middle);
 	vp.add(footer);
+
+        WebAPI service = GWT.create(WebAPI.class);
+        service.getPet(1, new MethodCallback<String>() {
+    
+                public void onSuccess(Method method, String response) {
+                    footer.add(new Label("onSuccess"));
+                }
+    
+                public void onFailure(Method method, Throwable exception) {
+                    footer.add(new Label(exception.getMessage()));
+                }
+            });
 
 	RootPanel rp = RootPanel.get();
 	rp.add(vp);
